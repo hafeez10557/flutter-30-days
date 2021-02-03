58945d4f30fe717b7921e1507114373e2334bc55
Abdul HAfeez <a.hafeez10557@gmail.com>
2021-02-03 19:34:42 +0500

    day2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("HI There"),
-        
+        centerTitle: true,
       ),
       body: Container(child: Text("Hi There")),
       drawer: Drawer(),
