5c15d6ef66e1a07a52f8dde76dfc1bcd6878cd21
Abdul HAfeez <a.hafeez10557@gmail.com>
2021-02-04 21:54:15 +0500

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hellow_world/pages/home_page.dart';
 import 'package:flutter_hellow_world/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.lailaTextTheme(),
+          ),
       routes: {
-        "/": (context) => HomePage(),
+        "/": (context) => LoginPage(),
         "/login": (context) => LoginPage(),
       },
     );
