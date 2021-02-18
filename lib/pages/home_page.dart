16be70f7569c94f88c8199fa2f010e0887da5f5b
Abdul HAfeez <a.hafeez10557@gmail.com>
2021-02-18 11:22:22 +0500

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_hellow_world/models/Product.dart';
 import 'package:flutter_hellow_world/widgets/Item_wegit.dart';
 import 'package:flutter_hellow_world/widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  loadData()async{
+    var catalogJSON=await rootBundle.loadString("assets/files.catalog.json");
+    var decdeData=jsonDecode(catalogJSON);
+    var productData=decdeData["products"];
+
+  }
   @override
   Widget build(BuildContext context) {
     final dumuList=List.generate(20, (index) => CatalogModel.item[0]);
+
 
     return Scaffold(
 
@@ -22,7 +42,7 @@ class HomePage extends StatelessWidget {
         },
       ),
       drawer: MyDrawer(),
-      
+
     );
   }
 }
